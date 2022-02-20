c6ad8e7ba14c2992cd11f743b6dc8832090acb0c
nadya <nadya.vilboa@list.ru>
2022-02-20 13:40:24 +0300

    Check mistakes

--- project/src/components/nav-container/nav-container.tsx
+++ project/src/components/nav-container/nav-container.tsx
@@ -1,6 +1,6 @@
-import NavAuth from '../nav-auth/nav-auth';
-import NavNoauth from '../nav-noauth/nav-noauth';
-import { AuthorizationStatus} from '../../const/routing';
+import { AppRoute, AuthorizationStatus} from '../../const/routing';
+import {Link} from 'react-router-dom';
+import SignOut from '../sign-out/sign-out';
 
 type NavContainerProps = {
   className: string;
@@ -8,12 +8,21 @@ type NavContainerProps = {
 }
 
 function NavContainer({className, authStatus}: NavContainerProps): JSX.Element {
-
+  const isAuth = authStatus === AuthorizationStatus.Auth;
   return (
     <nav className={className}>
-      {authStatus === AuthorizationStatus.Auth
-        ? <NavAuth className={''} />
-        : <NavNoauth className={''} />}
+      <ul className="header__nav-list">
+        <li className="header__nav-item user">
+          <Link to={isAuth ? AppRoute.Favorites : AppRoute.SignIn} className="header__nav-link header__nav-link--profile">
+            <div className="header__avatar-wrapper user__avatar-wrapper" />
+            <span className="header__user-name user__name">
+              {isAuth ? 'Oliver.conner@gmail.com' : 'Sign in'}
+            </span>
+          </Link>
+        </li>
+        {isAuth ?
+          <SignOut className='header__nav-item' /> : ''}
+      </ul>
     </nav>
   );
 }
--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,15 +1,18 @@
 import {Link} from 'react-router-dom';
 import Header from '../../components/header/header';
 import {AppRoute} from '../../const/routing';
+import styles from './not-found.module.css';
 
 function NotFound(): JSX.Element {
   return (
     <div className="page page--gray page--main">
       <Header />
-      <div className="not-found">
-        <h1>404. Page not found</h1>
-        <Link to={AppRoute.Main}>Вернуться на главную</Link>
-      </div>
+      <section className={styles.wrapper}>
+        <h1 className={styles.heading}>🙁 404. Page not found</h1>
+        <Link to={AppRoute.Main} className={styles.linkMain}>
+          Вернуться на главную
+        </Link>
+      </section>
     </div>
   );
 }
